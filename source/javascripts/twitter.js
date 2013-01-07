cef8cd2b19718b74c168ed6fea19ead9dbb7a2d2
Cameron Banga <cameronbanga@gmail.com>
2013-01-06 23:11:20 -0600

    trying to fix Twitter

--- source/javascripts/twitter.js
+++ source/javascripts/twitter.js
@@ -7,15 +7,15 @@ function prettyDate(time) {
     return "<span>&infin;</span>"; // because IE date parsing isn't fun.
   }
   var say = {
-    just_now:    " now",
-    minute_ago:  "1m",
-    minutes_ago: "m",
-    hour_ago:    "1h",
-    hours_ago:   "h",
-    yesterday:   "1d",
-    days_ago:    "d",
-    last_week:   "1w",
-    weeks_ago:   "w"
+    just_now:    " now - ",
+    minute_ago:  "1m - ",
+    minutes_ago: "m - ",
+    hour_ago:    "1h - ",
+    hours_ago:   "h - ",
+    yesterday:   "1d - ",
+    days_ago:    "d - ",
+    last_week:   "1w - ",
+    weeks_ago:   "w - "
   };
 
   var current_date = new Date(),
